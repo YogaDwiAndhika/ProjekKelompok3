33fe24b8f69e01d44aeb5b9b3510c07a812dbd4f
YogaDwiAndhika <yogadwiandhika10@gmail.com>
2025-06-14 18:51:38 +0700

    error fix#7

--- resources/views/detailproyek/index.blade.php
+++ resources/views/detailproyek/index.blade.php
@@ -18,7 +18,7 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($detailproyek as $dp)
+            @foreach($detailProyek as $dp)
             <tr>
                 <td>{{ $dp->proyek->NamaProyek ?? '-' }}</td>
                 <td>{{ $dp->bahan->NamaBahan ?? '-' }}</td>
